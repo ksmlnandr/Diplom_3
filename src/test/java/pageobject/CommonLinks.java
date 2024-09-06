49d86b90e072390c6ff2356cee552971688ecaa4
Andrei Kosmylin <kosmylin-andrei@yandex.ru>
2024-09-07 00:15:06 +0300

    добавлен класс CommonLinks

---
+++ src/test/java/pageobject/CommonLinks.java
@@ -0,0 +1,8 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonLinks {
+    private WebDriver driver;
+    private final String MAIN_PAGE_URL = "https://stellarburgers.nomoreparties.site";
+}
